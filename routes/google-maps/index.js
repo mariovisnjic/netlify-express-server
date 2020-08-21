c121c5d0711bdeb841991c42714ac6e79924afe9
mariovisnjic <mariovisnjic@hotmail.com>
2020-08-21 14:49:56 +0200

    route cleanup

--- routes/google-maps/index.js
+++ routes/google-maps/index.js
@@ -1,18 +1,7 @@
 const router = require('express').Router();
-//const Mood = require('../../models/mood')
 require('dotenv').config()
 
 router.get('/', (req, res) => {
-   /*var query = Mood.findOne();
-
-    query.exec(function (err, person) {
-        if (err) return handleError(err);
-        console.log("tu");
-        console.log(person);
-    });
-
-    Mood.create({'fullname':'denis'})*/
-
     res.send(process.env.GOOGLE_MAPS_API_KEY)
 })
 
